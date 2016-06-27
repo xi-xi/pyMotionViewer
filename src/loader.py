c5c9041aa73f82a13c234157f6e964e18cc20081
IDlab <idlab.saba@gmail.com>
2016-06-27 22:08:09 +0900

    [add]: add base load system

--- src/loader.py
+++ src/loader.py
@@ -1,2 +1,41 @@
+import os
 import pandas as pd
 import numpy as np
+import dataframe as df
+
+
+def load(filename, datatype=None):
+    if datatype is None:
+        datatype = os.path.splitext(filename)[1][1:].lower()
+    loader = get_loader(datatype)
+    if loader is None:
+        raise ValueError("datatype{} is invalid".format(datatype))
+    return loader(filename)
+
+
+def get_loader(datatype):
+    if datatype == "csv":
+        return load_csv
+    elif datatype == "trc":
+        return load_trc
+    elif datatype == "ts":
+        return load_ts
+    elif datatype == "xlsx":
+        return load_xlsx
+    return None
+
+
+def load_csv(filename):
+    pass
+
+
+def load_trc(filename):
+    pass
+
+
+def load_ts(filename):
+    pass
+
+
+def load_xlsx(filename):
+    pass
