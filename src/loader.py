3b4b62edc09d2fc5b85d03e003e53983a14f3b68
IDlab <idlab.saba@gmail.com>
2016-06-27 22:26:18 +0900

    [add]: add csv support

--- src/loader.py
+++ src/loader.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import pandas as pd
 import numpy as np
 import dataframe as df
@@ -26,7 +27,29 @@ def get_loader(datatype):
 
 
 def load_csv(filename):
-    pass
+    SEP = ','
+    m = df.Motion()
+    with open(filename, "r") as f:
+        prop_names = f.readline().split(SEP)
+        prop_values = f.readline().split(SEP)
+        if len(prop_names) != len(prop_values):
+            return None
+        props = dict(zip(prop_names, prop_values))
+        m.set_max_frame(int(props["NumFrames"]))
+        m.set_fps(float(props["DataRate"]))
+        m.set_markers(f.readline().split(SEP))
+        for row in csv.reader(f):
+            frame, posture = build_posture_from_csv(row, m.get_markers())
+            m[frame] = posture
+    return m
+
+
+def build_posture_from_csv(row, markers):
+    p = df.Posture()
+    frame = int(row[0])
+    for name, x, y, z in zip(markers[2::3], row[2::3], row[3::3], row[4::3]):
+        p[name] = np.array([float(x), float(y), float(z)])
+    return frame, p
 
 
 def load_trc(filename):
