4887a78b18ef4ade5d1490728686a3cba538a6b4
IDlab <idlab.saba@gmail.com>
2016-06-27 21:26:17 +0900

    [add]: add Motion class

--- src/dataframe.py
+++ src/dataframe.py
@@ -6,7 +6,34 @@
 
 class Motion:
     def __init__(self):
-        pass
+        self._postures = {}
+        self._markers = []
+        self._fps = 0.0
+        self._max_frame = 0
+
+    def __getitem__(self, key):
+        return self._postures[key]
+
+    def __setitem__(self, key, value):
+        self._postures[key] = value
+
+    def get_fps(self):
+        return self._fps
+
+    def set_fps(self, value):
+        self._fps = value
+
+    def get_max_frame(self):
+        return self._max_frame
+
+    def set_max_frame(self, value):
+        self._max_frame = value
+
+    def get_markers(self):
+        return self._markers
+
+    def set_markers(self, value):
+        self._markers = value
 
 
 class Posture:
